803f1c6b9510c185f22c36c1188df8289a457ce8
kodoukodou <yoshi040120@gmail.com>
2023-06-05 16:31:55 +0900

    0605

--- Assets/FourSides.cs
+++ Assets/FourSides.cs
@@ -9,33 +9,22 @@ public class FourSides : EditorWindow
     private Camera cam;
     private Camera view;
     private Camera[] camera = new Camera[4];
-    private Camera front;
-    private Camera back;
-    private Camera right;
-    private Camera left;
     private GameObject[] pl = new GameObject[4];
-    private GameObject plane;
-    private GameObject front_plane;
-    private GameObject back_plane;
-    private GameObject right_plane;
-    private GameObject left_plane;
-
-    private Material[] material=new Material[4];
+    private GameObject[] light = new GameObject[5];
+    private Material[] material = new Material[4];
     private String[] place = new String[4];
+    private RenderTexture[] rt = new RenderTexture[4];
 
-    private RenderTexture[] rt=new RenderTexture[4];
-    private RenderTexture front_rt;
-    private RenderTexture back_rt;
-    private RenderTexture right_rt;
-    private RenderTexture left_rt;
-
+    private GameObject plane;
     private Vector3 center = new Vector3(5, 0, 0);
-
     private float height = 2.5f;
     private float cam_size = 2.5f;
     private int render_size=1080;
     private int current_render_size ;
     private bool under = false;
+
+    private float light_height=(float)0.8;
+    private float light_distance= (float)6.5;
 
 
     [MenuItem("MyMenu/Create/FourSides")]
@@ -50,21 +39,6 @@ public class FourSides : EditorWindow
         place[1] = "right";
         place[2] = "front";
         place[3] = "left";
-
-        camera[0] = back;
-        camera[1] = right;
-        camera[2] = front;
-        camera[3] = left;
-
-        rt[0] = back_rt;
-        rt[1] = right_rt;
-        rt[2] = front_rt;
-        rt[3] = left_rt;
-
-        pl[0] = back_plane;
-        pl[1] = right_plane;
-        pl[2] = front_plane;
-        pl[3] = left_plane;
     }
     private void OnGUI()
     {
@@ -81,6 +55,15 @@ public class FourSides : EditorWindow
         {
             height = EditorGUILayout.FloatField("*height", height);
             cam_size = EditorGUILayout.FloatField("*cam_size", cam_size);
+        }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.LabelField("Light");
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        {
+            light_height = EditorGUILayout.FloatField("*height", light_height);
+            light_distance = EditorGUILayout.FloatField("*distance", light_distance);
         }
         EditorGUILayout.EndVertical();
 
@@ -114,13 +97,18 @@ public class FourSides : EditorWindow
                 cam.farClipPlane = 5;
                 cam.orthographicSize = cam_size;
 
+                //ライト生成と設定
+                // Make a game object
+                GameObject lightGameObject = new GameObject("The Light");
+
+                // Add the light component
+                Light lightComp = lightGameObject.AddComponent<Light>();
+                lightComp.type = LightType.Directional;
+
+
                 //親子設定
                 var parent = new GameObject("CharaCamera").transform;
-
-                /*camera[0] = cam;
-                camera[0].targetTexture = rt[0];
-                camera[0].transform.parent = parent.transform;
-                camera[0].name = string.Format("{0}_cam", place[0]);*/
+                var parent2 = new GameObject("Light").transform;
 
                 //Mainカメラ
                 view = Camera.main;
@@ -144,36 +132,51 @@ public class FourSides : EditorWindow
                 //Planeの生成と設定
                 plane.transform.position = center;
 
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < 5; i++)
                 {
-                    //カメラ
-                    camera[i] = Instantiate(cam);
-                    camera[i].name = string.Format("{0}_cam", place[i]);
-                    camera[i].targetTexture = rt[i];
-                    camera[i].transform.parent = parent.transform;
-                    camera[i].backgroundColor = Color.black;
-                    //板
-                    pl[i]= Instantiate(plane);
-                    pl[i].name = string.Format("{0}_plane", place[i]);
-                    pl[i].transform.rotation = Quaternion.Euler(0, i*90, 0);
-                    material[i].SetTexture("_MainTex", rt[i]);
-                    pl[i].GetComponent<MeshRenderer>().material = material[i];
+                    if (i < 4) { 
+                        //カメラ
+                        camera[i] = Instantiate(cam);
+                        camera[i].name = string.Format("{0}_cam", place[i]);
+                        camera[i].targetTexture = rt[i];
+                        camera[i].transform.parent = parent.transform;
+                        camera[i].backgroundColor = Color.black;
+                        //板
+                        pl[i]= Instantiate(plane);
+                        pl[i].name = string.Format("{0}_plane", place[i]);
+                        pl[i].transform.rotation = Quaternion.Euler(0, i*90, 0);
+                        material[i].SetTexture("_MainTex", rt[i]);
+                        pl[i].GetComponent<MeshRenderer>().material = material[i];
+                        //ライト
+                        light[i] = Instantiate(lightGameObject);
+                        light[i].name = string.Format("{0}_light", place[i]);
+                        light[i].transform.parent = parent2.transform;
+                        light[i].transform.rotation = Quaternion.Euler(0, i * 90, 0);
+                    }else
+                    {
+                        light[i] = Instantiate(lightGameObject);
+                        light[i].name = "view_light";
+                        light[i].transform.parent = parent2.transform;
+                        light[i].transform.rotation = Quaternion.Euler(90, 0, 0);
+                    }
                 }
 
                 DestroyImmediate(obj);
+                DestroyImmediate(lightGameObject);
                 Under();
 
                 //親子設定
-                var parent2 = new GameObject("Plane").transform;
+                var parent3 = new GameObject("Plane").transform;
                 for (int i = 0; i < 4; i++)
                 {
-                    pl[i].transform.parent = parent2.transform;
+                    pl[i].transform.parent = parent3.transform;
                 }
 
                 //親子設定
-                var parent3 = new GameObject("FourSides").transform;
-                parent.transform.parent = parent3.transform;
-                parent2.transform.parent = parent3.transform;
+                var parent4 = new GameObject("FourSides").transform;
+                parent.transform.parent = parent4.transform;
+                parent2.transform.parent = parent4.transform;
+                parent3.transform.parent = parent4.transform;
             }
 
             //置き直し
@@ -208,6 +211,13 @@ public class FourSides : EditorWindow
 
     void Under()
     {
+        //ライト位置
+        light[0].transform.position = avater.transform.position + new Vector3(0, light_height, -light_distance);
+        light[1].transform.position = avater.transform.position + new Vector3(-light_distance, light_height, 0);
+        light[2].transform.position = avater.transform.position + new Vector3(0, light_height, light_distance);
+        light[3].transform.position = avater.transform.position + new Vector3(light_distance, light_height, 0);
+        light[4].transform.position = center + new Vector3(0, (float)6.5, 0);
+
         //上か下か
         if (!under)
         {
