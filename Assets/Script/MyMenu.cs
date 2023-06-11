84b36bdfaa25703cb6ea0531cef19f6e0537c863
kodoukodou <yoshi040120@gmail.com>
2023-06-11 16:56:00 +0900

    0611

--- Assets/Script/MyMenu.cs
+++ Assets/Script/MyMenu.cs
@@ -6,7 +6,7 @@ public class MyMenu : MonoBehaviour
     private static Camera _cam;
 
     //  Add a menu item named "Create/Camera" to McGUI in the menu bar.
-    [MenuItem("MyMenu/Create/Camera")]
+    //[MenuItem("MyMenu/Create/Camera")]
     static void CreateCamera()
     {
         GameObject obj = UnityEditor.EditorUtility.CreateGameObjectWithHideFlags("Camera", HideFlags.None, typeof(Camera));
